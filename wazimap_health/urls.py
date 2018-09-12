01943088c6d10b2ec9c914098d48c60ad738053c
lunga <stumenz.complex@gmail.com>
2018-09-12 12:17:13 +0200

    add api urls

--- wazimap_health/urls.py
+++ wazimap_health/urls.py
@@ -2,12 +2,17 @@ from django.conf.urls import url, patterns, include
 from django.contrib import admin
 from wazimap import urls
 
-# from api import views
+from api import views as api
+from . import views
 
 urlpatterns = patterns('',
-                       # url(r'api/point/v1/facilities$',
-                       #     views.FacilityView.as_view(),
-                       #     name='health_facility'),
+                       url(r'api/point/v1/facilities$',
+                           api.PublicFacilityView.as_view(),
+                           name='health_facility'),
+                       url(r'api/point/v1/facilities/(?P<code>\w+)/services$',
+                           api.PublicServicesView.as_view(),
+                           name='health_services'),
+                       url(r'point/profile$', views.point_detail),
                        url(r'^admin/', include(admin.site.urls)))
 
 urlpatterns += urls.urlpatterns
