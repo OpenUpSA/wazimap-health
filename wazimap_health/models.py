af55472672445e44c586afc986a40bb0c2d97ae4
lunga <stumenz.complex@gmail.com>
2018-09-12 17:21:32 +0200

    add pharamcies and marie stopes datasets

--- wazimap_health/models.py
+++ wazimap_health/models.py
@@ -1,6 +1,86 @@
 from django.db import models
 
 
+class PrivatePharmacies(models.Model):
+    name = models.CharField(max_length=100)
+    settlement = models.CharField(max_length=100, blank=True)
+    unit = models.CharField(max_length=100, blank=True)
+    latitude = models.DecimalField(decimal_places=5, max_digits=7, blank=True)
+    longitude = models.DecimalField(decimal_places=5, max_digits=7, blank=True)
+    parent_geo_code = models.CharField(max_length=10, blank=True)
+    facility_code = models.CharField(max_length=20, unique=True)
+    address = models.CharField(max_length=150, blank=True)
+
+    class Meta:
+        db_table = 'private_pharmacies'
+        unique_together = ('name', 'latitude', 'longitude')
+
+
+class PharmacyServices(models.Model):
+    facility = models.ForeignKey(PrivatePharmacies)
+    oral_pills = models.BooleanField(verbose_name='Oral Pills (Contraception)', blank=True)
+    injectables = models.BooleanField(blank=True)
+    iud = models.BooleanField(verbose_name='IUDs (Contraception)', blank=True)
+    female_sterialization = models.BooleanField(verbose_name='Female Sterilization (Contraception)', blank=True)
+    male_sterialization = models.BooleanField(verbose_name='Male Sterilization (Contraception)', blank=True)
+    male_circumcision = models.BooleanField(verbose_name='Male Medical Circumcision (MMC)')
+    tb = models.BooleanField(verbose_name="TB", blank=True)
+    maternal_health = models.BooleanField(verbose_name='Maternal Health', blank=True)
+    mental_health = models.BooleanField(verbose_name='Mental Health', blank=True)
+    child_health = models.BooleanField(verbose_name='Child Health', blank=True)
+    oral_health = models.BooleanField(verbose_name='Oral Health Services',
+                                      blank=True)
+    rehabilitation = models.BooleanField(verbose_name='Rehabilitation Services',
+                                         blank=True)
+    minor_ailments = models.BooleanField(verbose_name='Minor Ailments', blank=True)
+    std = models.BooleanField(verbose_name='Sexually Transmitted infections Screenings', blank=True)
+    hiv_testing = models.BooleanField(verbose_name='HIV Testing', blank=True)
+    hiv_treatment = models.BooleanField(verbose_name='HIV Treatment (ART)', blank=True)
+    oral_prep = models.BooleanField(verbose_name='Oral PrEP', blank=True)
+    first_trimester = models.BooleanField(verbose_name='Termination of Pregnancy - 1st Trimester', blank=True)
+    second_trimester = models.BooleanField(verbose_name='Termination of Pregnancy - 2nd Trimester', blank=True)
+    implants = models.BooleanField(blank=True, verbose_name='Implants (Contraception)')
+
+    class Meta:
+        db_table = 'pharmacy_services'
+
+
+class MarieStopes(models.Model):
+    name = models.CharField(max_length=100)
+    settlement = models.CharField(max_length=100, blank=True)
+    latitude = models.DecimalField(decimal_places=5, max_digits=7, blank=True)
+    longitude = models.DecimalField(decimal_places=5, max_digits=7, blank=True)
+    parent_geo_code = models.CharField(max_length=10, blank=True)
+    facility_code = models.CharField(max_length=20, unique=True)
+    address = models.CharField(max_length=100, blank=True)
+
+    class Meta:
+        db_table = 'marie_stopes_facilities'
+        unique_together = ('name', 'latitude', 'longitude')
+
+    def __str__(self):
+        return self.name
+
+
+class MarieStopesFacilities(models.Model):
+    facility = models.ForeignKey(MarieStopes)
+    oral_pills = models.BooleanField(verbose_name='Oral Pills (Contraception)', blank=True)
+    injectables = models.BooleanField(blank=True)
+    iud = models.BooleanField(verbose_name='IUDs (Contraception)', blank=True)
+    female_sterialization = models.BooleanField(verbose_name='Female Sterilization (Contraception)', blank=True)
+    male_sterialization = models.BooleanField(verbose_name='Male Sterilization (Contraception)', blank=True)
+    std = models.BooleanField(verbose_name='Sexually Transmitted infections Screenings', blank=True)
+    hiv_testing = models.BooleanField(verbose_name='HIV Testing', blank=True)
+    hiv_treatment = models.BooleanField(verbose_name='HIV Treatment (ART)', blank=True)
+    oral_prep = models.BooleanField(verbose_name='Oral PrEP', blank=True)
+    first_trimester = models.BooleanField(verbose_name='Termination of Pregnancy - 1st Trimester', blank=True)
+    second_trimester = models.BooleanField(verbose_name='Termination of Pregnancy - 2nd Trimester', blank=True)
+    implants = models.BooleanField(blank=True, verbose_name='Implants (Contraception)')
+
+    class Meta:
+        db_table = 'marie_stopes_services'
+
+
 class PublicHealthFacilities(models.Model):
     name = models.CharField(max_length=100)
     settlement = models.CharField(max_length=100, blank=True)
@@ -9,8 +89,6 @@ class PublicHealthFacilities(models.Model):
     longitude = models.DecimalField(decimal_places=5, max_digits=7, blank=True)
     parent_geo_code = models.CharField(max_length=10)
     facility_code = models.CharField(max_length=20, unique=True)
-
-    #stats = PublicHealthQuerySet.as_manager()
 
     class Meta:
         db_table = 'public_health_facilities'
