4a16aa79e3a9818442ee735cd69cd87c3c063044
lunga <stumenz.complex@gmail.com>
2018-09-12 12:12:43 +0200

    more management commands

--- wazimap_health/management/commands/public_health_services.py
+++ wazimap_health/management/commands/public_health_services.py
@@ -1,16 +1,1 @@
-from django.core.management.base import BaseCommand, CommandError
 
-from wazimap_health.models import PublicHealthFacilities
-
-
-class Command(BaseCommand):
-    help = "Import the services that a public health facility provides"
-
-    def add_arguments(self, parser):
-        parser.add_argument('file', type=str)
-
-    def handle(self, *args, **kwargs):
-        """
-        Go through each facility
-        """
-        return
