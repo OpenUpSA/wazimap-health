8d274419c87ec18130d96018c73af6dbb93e2df9
lunga <stumenz.complex@gmail.com>
2018-09-12 17:24:24 +0200

    new datasets

--- wazimap_health/urls.py
+++ wazimap_health/urls.py
@@ -12,6 +12,9 @@ urlpatterns = patterns('',
                        url(r'api/point/v1/facilities/(?P<code>\w+)/services$',
                            api.PublicServicesView.as_view(),
                            name='health_services'),
+                       url(r'api/point/v1/pharmacies$',
+                           api.PharmacyView.as_view(),
+                           name='private_pharmacy'),
                        url(r'point/profile$', views.point_detail),
                        url(r'^admin/', include(admin.site.urls)))
 
