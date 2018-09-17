ac12d3c82a06bf5b2b4331baaa6be75e4fe2f1fa
lunga <stumenz.complex@gmail.com>
2018-09-17 10:56:07 +0200

    add api for the facility code

--- wazimap_health/api/serializers.py
+++ wazimap_health/api/serializers.py
@@ -1,41 +1,10 @@
 from rest_framework import serializers
 
-from wazimap_health.models import (PublicHealthFacilities,
-                                   PublicHealthServices,
-                                   PrivatePharmacies,
-                                   PharmacyServices)
+from wazimap_health.models import HealthFacilities
 
 
-class PublicHealthFacilitySerializer(serializers.ModelSerializer):
+class HealthFacilitySerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = PublicHealthFacilities
-        fields = '__all__'
-
-
-class PharmacySerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = PrivatePharmacies
-        fields = '__all__'
-
-
-# class ServicesModelSerializer(serializers.ModelSerializer):
-#     def __init__(self, *args, **kwargs):
-#         super(ServicesModelSerializer, self).__init__(*args, **kwargs)
-
-#         if 'lables' in self.fields:
-#             raise RuntimeError('No labels allowed in this serializer')
-#         self.fields['lables'] = serializers.SerializerMethodField()
-
-#     def get_lables(self, args):
-#         labels = {}
-#         for field in self.Meta.model._meta.get_fields():
-#             if field.name in self.fields:
-#                 labels[field.name] = field.verbose_name
-#         return labels
-
-# class PublicServicesSerializer(ServicesModelSerializer):
-#     class Meta:
-#         model = PublicHealthServices
-#         fields = '__all__'
+        model = HealthFacilities
+        exclude = ('service', 'id')
--- wazimap_health/api/views.py
+++ wazimap_health/api/views.py
@@ -1,69 +1,33 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
-from wazimap_health.models import (PublicHealthFacilities,
-                                   PublicHealthServices,
-                                   MarieStopes,
-                                   MarieStopesFacilities,
-                                   PrivatePharmacies,
-                                   PharmacyServices)
+from wazimap_health.models import HealthFacilities
 
 from . import serializers
 
 
-class PharmacyView(APIView):
+class HealthFacilityView(APIView):
     """
-    Show all the phramacies within a district
+    Show all the health facilities within a district
     """
     def get(self, request):
         geo_code = request.query_params.get('geo_code', None)
+        facility_code = request.query_params.get('facility_code', None)
         if geo_code:
-            query = PrivatePharmacies\
-                    .objects\
-                    .filter(parent_geo_code=geo_code)
-            serialize = serializers.PharmacySerializer(query, many=True)
+            query = HealthFacilities.objects.filter(parent_geo_code=geo_code)
+            serialize = serializers.HealthFacilitySerializer(query, many=True)
             return Response(
-                {'data': serialize.data}
+                {
+                    'data': serialize.data
+                }
             )
-        return Response(status.HTTP_400_BAD_REQUEST)
-
-
-class PublicFacilityView(APIView):
-    """
-    Show all the Public health Facilities within a Geography
-    """
-    def get(self, request):
-        parent_geo_code = request.query_params.get('parent_geo_code', None)
-        facility = request.query_params.get('facility', None)
-        if parent_geo_code is None and facility is None:
-            return Response(status.HTTP_400_BAD_REQUEST)
-        if parent_geo_code:
-            query = PublicHealthFacilities\
-                    .objects\
-                    .filter(parent_geo_code=parent_geo_code)
-            serialize = serializers.PublicHealthFacilitySerializer(query,
-                                                               many=True)
+        elif facility_code:
+            query = HealthFacilities.objects.get(facility_code=facility_code)
+            serialize = serializers.HealthFacilitySerializer(query)
             return Response(
-                {'data': serialize.data}
+                {
+                    'data': serialize.data
+                }
             )
-        if (facility):
-            query = PublicHealthFacilities\
-                    .objects\
-                    .get(facility_code=facility)
-            serialize = serializers.PublicHealthFacilitySerializer(query)
-            return Response({
-                'data': serialize.data
-            })
-
-
-class PublicServicesView(APIView):
-    """
-    Show all the services a particular health service has
-    """
-    def get(self, request, code):
-        query = PublicHealthServices\
-                .objects\
-                .get(facility__facility_code=code)
-        serialize = serializers.PublicServicesSerializer(query)
-        return Response(serialize.data)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
