2312e4833c0f42719e681f378330967e6730b223
lunga <stumenz.complex@gmail.com>
2018-09-17 11:05:38 +0200

    we only need 1 model now.

--- wazimap_health/management/commands/geocode_facility.py
+++ wazimap_health/management/commands/geocode_facility.py
@@ -1,7 +1,7 @@
 import requests
 
 from django.core.management.base import BaseCommand, CommandError
-from wazimap_health.models import PublicHealthFacilities, MarieStopes, PrivatePharmacies
+from wazimap_health.models import HealthFacilities
 
 MAPIT_URL = "http://10.186.210.96:8000/point/4326/"
 
@@ -9,18 +9,12 @@ MAPIT_URL = "http://10.186.210.96:8000/point/4326/"
 class Command(BaseCommand):
     help = "Search and import a geo code for a facility"
 
-    def add_arguments(self, parser):
-        parser.add_argument('model', type=str)
-
     def handle(self, *args, **options):
         """
         Get the district geo code from mapit based on the faclities lat/lon
         """
-        if options['model'] is None:
-            raise CommandError('No Model specified')
         try:
-            model = eval(options['model'])
-            for facility in model.objects.all():
+            for facility in HealthFacilities.objects.all():
                 lat = facility.latitude
                 lon = facility.longitude
                 url = MAPIT_URL + '{},{}?type=DC'.format(lon, lat)
@@ -32,6 +26,6 @@ class Command(BaseCommand):
                         facility.save(update_fields=['parent_geo_code'])
                         self.stdout.write(self.style.SUCCESS("Geo Inserted"))
                 else:
-                    self.stdout.write(self.style.SUCCESS("No District found for"))
+                    self.stdout.write(self.style.ERROR("No District found for"))
         except Exception as error:
             raise CommandError(error)
