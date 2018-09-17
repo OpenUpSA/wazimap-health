b52d46789661e3c6e8594ad811b6676308bef2d8
lunga <stumenz.complex@gmail.com>
2018-09-17 11:00:34 +0200

    remove uneeded urls

--- wazimap_health/urls.py
+++ wazimap_health/urls.py
@@ -3,19 +3,12 @@ from django.contrib import admin
 from wazimap import urls
 
 from api import views as api
-from . import views
 
 urlpatterns = patterns('',
-                       url(r'api/point/v1/facilities$',
-                           api.PublicFacilityView.as_view(),
+                       url(r'^api/point/v1/facilities$',
+                           api.HealthFacilityView.as_view(),
                            name='health_facility'),
-                       url(r'api/point/v1/facilities/(?P<code>\w+)/services$',
-                           api.PublicServicesView.as_view(),
-                           name='health_services'),
-                       url(r'api/point/v1/pharmacies$',
-                           api.PharmacyView.as_view(),
-                           name='private_pharmacy'),
-                       url(r'point/profile$', views.point_detail),
+                       #url(r'^api/point/v1/facilities/(?P<geo_code>\w+)/services$'),
                        url(r'^admin/', include(admin.site.urls)))
 
 urlpatterns += urls.urlpatterns
