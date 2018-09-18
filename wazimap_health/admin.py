960c38d5be5a81820eebc739575782ed5c64ac84
lunga <stumenz.complex@gmail.com>
2018-09-18 08:47:30 +0200

    renaming

--- wazimap_health/admin.py
+++ wazimap_health/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
 from django import forms
 from django_admin_hstore_widget.forms import HStoreFormField
 
 from . import models
+
+
+class HealthAdminSite(AdminSite):
+    site_title = 'Clinton Health Access Initiative'
+    site_header = 'CHAI Administration'
+    index_title = 'CHAI Admin'
+
+
+admin_site = HealthAdminSite()
 
 
 class HealthFacilityAdminForm(forms.ModelForm):
@@ -13,10 +23,12 @@ class HealthFacilityAdminForm(forms.ModelForm):
         exclude = ()
 
 
-@admin.register(models.HealthFacilities)
 class HealthFacilityAdmin(admin.ModelAdmin):
     list_display = ('name', 'settlement', 'unit', 'facility_code',
                     'dataset',
                     'latitude', 'longitude')
     list_filter = ('dataset',)
     form = HealthFacilityAdminForm
+
+
+admin_site.register(models.HealthFacilities, HealthFacilityAdmin)
--- wazimap_health/urls.py
+++ wazimap_health/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, patterns, include
 from django.contrib import admin
+from .admin import admin_site
 from wazimap import urls
 
 from api import views as api
@@ -9,6 +10,6 @@ urlpatterns = patterns('',
                            api.HealthFacilityView.as_view(),
                            name='health_facility'),
                        #url(r'^api/point/v1/facilities/(?P<geo_code>\w+)/services$'),
-                       url(r'^admin/', include(admin.site.urls)))
+                       url(r'^admin/', include(admin_site.urls)))
 
 urlpatterns += urls.urlpatterns
