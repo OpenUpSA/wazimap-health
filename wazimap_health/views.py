2c0316de2b654d3f0aa30aee43f90abffc9b5734
lunga <stumenz.complex@gmail.com>
2019-01-18 14:54:54 +0200

    make sure activities can be seem on a sub provincial level

--- wazimap_health/views.py
+++ wazimap_health/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.db.models import Q
 
-from .models import Organisation, Activity, PartnerBasicEducation, PartnerHealth, PartnerHigherEducation
+from .models import (Organisation, Activity, PartnerBasicEducation,
+                     PartnerHealth, PartnerHigherEducation, PartnerLocation)
 from wazimap.models import Geography
 
 
@@ -12,7 +13,7 @@ def organisation_profile(request, geo_name, org_slug):
     organisation = Organisation\
                    .objects\
                    .get(slug=org_slug)
-    province = Geography\
+    geo_area = Geography\
                .objects\
                .values('geo_code')\
                .get(name=geo_name, version='2011')
@@ -22,21 +23,29 @@ def organisation_profile(request, geo_name, org_slug):
                  .filter(Q(organisation__slug=org_slug),
                          Q(province=geo_name)| Q(province="All provinces"))
     activity_numbers = []
+    if not activities:
+        location_activities = PartnerLocation\
+                     .objects\
+                     .filter(location_name=geo_name, organisation=organisation)\
+                     .values('activity_number')
+        activity_numbers = [
+            act_no['activity_number'] for act_no in location_activities
+        ]
+        activities = Activity\
+                     .objects\
+                     .order_by('activity_number')\
+                     .filter(organisation__slug=org_slug,
+                             activity_number__in=activity_numbers)
+
     for activity in activities:
         activity_numbers.append(activity.activity_number)
 
     basic_ed = PartnerBasicEducation.objects.filter(
-        organisation=organisation,
-        activity_number__in=activity_numbers,
-        province=province['geo_code'])
+        organisation=organisation, activity_number__in=activity_numbers)
     higher_ed = PartnerHigherEducation.objects.filter(
-        organisation=organisation,
-        activity_number__in=activity_numbers,
-        province=province['geo_code'])
+        organisation=organisation, activity_number__in=activity_numbers)
     health = PartnerHealth.objects.filter(
-        organisation=organisation,
-        activity_number__in=activity_numbers,
-        province=province['geo_code'])
+        organisation=organisation, activity_number__in=activity_numbers)
 
     return render(
         request, 'organisation_profile.html', {
